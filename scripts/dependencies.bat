7a63d0e7deaff32db8d670d2a117909b8c549950
Germán Boquizo <xekevara@gmail.com>
2020-04-04 16:30:05 +0200

    Added script to install all project dependencies on Windows

---
+++ scripts/dependencies.bat
@@ -0,0 +1,7 @@
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+choco install -y python
+choco install -y nodejs
+choco install -y git
+npm install
+npm run development
+py -3 -m venv venv
