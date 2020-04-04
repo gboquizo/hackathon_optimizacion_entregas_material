189cb30f7d950aa3f21afcda5065bb002ccde51f
Germán Boquizo <xekevara@gmail.com>
2020-04-04 18:09:20 +0200

    Fix discovering images and fonts

--- webpack.mix.js
+++ webpack.mix.js
@@ -13,4 +13,11 @@ let mix = require('laravel-mix');
 mix.webpackConfig({ resolve: { modules: [__dirname, 'node_modules', 'resources/js'] } })
 
 mix.js('resources/js/app.js', 'app/static/js/app.js')
-  .sass('resources/scss/main.scss', 'app/static/css/app.css');
+  .sass('resources/scss/main.scss', 'app/static/css/app.css')
+  .options({
+    processCssUrls: true,
+      fileLoaderDirs: {
+         images: 'app/static/images',
+         fonts: 'app/static/fonts'
+      }
+  });
