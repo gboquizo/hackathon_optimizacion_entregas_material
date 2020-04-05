6ce4d08b04e39558bfa7b666411c204832a243b6
Guillermo <guillermoboquizosanchez@gmail.com>
2020-04-05 10:44:16 +0200

    modifying scripts to install all project dependencies on Windows via exe

--- scripts/pydependencies.bat
+++ scripts/pydependencies.bat
@@ -1,2 +1,5 @@
+@echo off
+title Delia's Installing Python Dependencies
+SET projectPath=%~dp0..
 echo %projectPath%
-cmd /k "cd /d %projectPath%\venv\Scripts & activate & cd /d %projectPath% & pip install -r requirements.txt && pipenv install"
+cmd /k "cd /d %projectPath%\venv\Scripts & activate & cd /d %projectPath% & pip install -r requirements.txt && pipenv install &&  deactivate && cd..&& clean.bat"
