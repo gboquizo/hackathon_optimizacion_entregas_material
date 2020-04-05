88b0204fbc61824b1bb661ace56791347f2e272d
Germán Boquizo Sánchez <xekevara@gmail.com>
2020-04-05 17:16:13 +0200

    Revert "First app layout"

--- app/forms.py
+++ app/forms.py
@@ -5,26 +5,26 @@ from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 class LoginForm(FlaskForm):
     """User Login Form."""
-    email = StringField('Correo electrónico', validators=[
-        Email('Por favor, introduce un correo electrónico válido.'),
-        DataRequired('Por favor, introduce un correo.')
+    email = StringField('Email', validators=[
+        Email('Please enter a valid email address.'),
+        DataRequired('Please enter a valid email address.')
     ])
-    password = PasswordField('Contraseña', validators=[DataRequired('Oh, ¿y la contraseña?')])
-    remember_me = BooleanField('Recuérdame')
-    submit = SubmitField('Entrar')
+    password = PasswordField('Password', validators=[DataRequired('Uhh, your password tho?')])
+    remember_me = BooleanField('Remember Me')
+    submit = SubmitField('Sign in')
 
 
 class RegisterForm(FlaskForm):
     """User Register Form."""
     email = StringField('Email', validators=[
-        Email('Por favor, introduce un correo electrónico válido.'),
-        DataRequired('Por favor, introduce un correo.')
+        Email('Please enter a valid email address.'),
+        DataRequired('Please enter a valid email address.')
     ])
-    username = StringField('Nombre de usuario', validators=[DataRequired('¡Introduce un nombre de usuario!')])
-    password = PasswordField('Contraseña', validators=[
-        DataRequired('Por favor, introduzca una contraseña.'),
-        Length(min=8, message='La contraseña tiene que ser más fuerte. Al menos 8 caracteres.'),
-        EqualTo('password_confirm', message='Las contraseñas tienen que coincidir')
+    username = StringField('Username', validators=[DataRequired('Enter a fake name or something.')])
+    password = PasswordField('Password', validators=[
+        DataRequired('Please enter a password.'),
+        Length(min=8, message='Please select a stronger password'),
+        EqualTo('password_confirm', message='Password must match')
     ])
-    password_confirm = PasswordField('Confirma tu contraseña')
-    submit = SubmitField('Registrarme')
+    password_confirm = PasswordField('Confirm your Password')
+    submit = SubmitField('Register')
