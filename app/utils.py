a99ec414d9164c91de557f42de87228182ab3ea7
Germán Boquizo <xekevara@gmail.com>
2020-04-05 19:28:23 +0200

    Restoring layout...

--- app/utils.py
+++ app/utils.py
@@ -7,16 +7,3 @@ from pprint import pprint
 def dd(variable):
     pprint(variable)
     sys.exit()
-
-
-def upload_path():
-    is_windows = os.name == 'nt'
-    directory = os.path.abspath('/opt/SGDF')
-
-    if is_windows:
-        directory = os.path.abspath('C:\\SGDF')
-
-    if not os.path.exists(directory):
-        os.makedirs(directory)
-
-    return directory
